a4bb344fc7cee95a21d0c1a7d0947ca1e7109b7d
LucyQ19 <lucyq19@gmail.com>
2019-04-06 06:57:57 -0500

    update html and js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -17,13 +17,13 @@
 
     var trainName = $("#trainName").val().trim();
     var trainDestination = $("#trainDestination").val().trim();
-    var firstTrainTime = $("#firstTrainTime").val().trim();
+    var trainTime = $("#trainTime").val().trim();
     var trainFrequency =$("#trainFrequency").val().trim();
 
     var newTrain = {
       name: trainName,
       destination: trainDestination,
-      time: firstTrainTime,
+      time: trainTime,
       frequency: trainFrequency
     };
 
@@ -36,7 +36,7 @@
 
     $("#trainName").val("");
     $("#trainDestination").val("");
-    $("firstTrainTime").val("");
+    $("trainTime").val("");
     $("#trainFrequency").val("");
 
   });
@@ -45,13 +45,40 @@
 
     var trainName = childSnapshot.val().name;
     var trainDestination = childSnapshot.val().destination;
-    var firstTrainTime = childSnapshot.val().time;
+    var trainTime = childSnapshot.val().time;
     var trainFrequency = childSnapshot.val().frequency;
 
     console.log(trainName);
     console.log(trainDestination);
-    console.log(firstTrainTime);
+    console.log(trainTime);
     console.log(trainFrequency);
 
+    var currentTime = moment();
+    console.log("Current time: " + moment(currentTime).format("hh:mm"));
+
+    var convertedTime = moment(trainTime, "HH:mm").subtract(1, "years");
+    console.log(convertedTime);
+
+    var diffTime = moment().diff(moment(convertedTime), "minutes");
+    console.log("Difference in Time: " + diffTime);
+
+    var timeRemainder = diffTime % trainFrequency;
+    console.log(timeRemainder);
+
+    var minutesAway = trainFrequency - timeRemainder;
+    console.log("Minutes Away: " + minutesAway);
+
+    var nextArrival = moment().add(minutesAway, "minutes");
+    console.log("Next Arrival: " + moment(nextArrival).format("hh:mm"));
+
+    var newRow = $("<tr>").append(
+      $("<td>").text(trainName),
+      $("<td>").text(trainDestination),
+      $("<td>").text(trainFrequency),
+      $("<td>").text(moment(nextArrival).format("hh:mm A")),
+      $("<td>").text(minutesAway)
+    );
+
+    $("#scheduleTable > tbody").append(newRow);  
 
   });
